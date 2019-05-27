b62a94585c4318059be7ef833a30e8b65107a960
Matthew Cook <matt@cook.company>
2019-05-27 13:30:12 -0500

    support google calendar

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ Schedule the display of a set of URLs via a public Google Sheet.
 
 ## Use
 
+### Google Calendar
+
+1. Create a new Google Calendar and set the visiblity settings to "See all event details" on "Make avaible to the public": [https://support.google.com/calendar/answer/37082?hl=en](https://support.google.com/calendar/answer/37082?hl=en)
+2. Create events using the desired URL as the event title. e.g. [https://calendar.google.com/calendar/embed?src=cook.company_mlqfhsl5mo9na67sf3vno0fjt8%40group.calendar.google.com&ctz=America%2FChicago](https://calendar.google.com/calendar/embed?src=cook.company_mlqfhsl5mo9na67sf3vno0fjt8%40group.calendar.google.com&ctz=America%2FChicago)
+3. Copy the ID of the Google Calendar. In the example above it is `cook.company_mlqfhsl5mo9na67sf3vno0fjt8%40group.calendar.google.com`
+4. Fill out this form with the appropriate values.
+5. Use the generated URL as the content of your kiosk or display.
+
+### Google Sheets
+
 1. Creat a Google Sheet with "start" and "url" columns. e.g. [https://docs.google.com/spreadsheets/d/17eiV1NY78hISTCsygjlXGPw9G6v75yTB5vB0IzRvmjA/edit?usp=sharing](https://docs.google.com/spreadsheets/d/17eiV1NY78hISTCsygjlXGPw9G6v75yTB5vB0IzRvmjA/edit?usp=sharing)
 2. Publicly publish the Google Sheet: [https://support.google.com/docs/answer/183965?co=GENIE.Platform%3DDesktop&hl=en](https://support.google.com/docs/answer/183965?co=GENIE.Platform%3DDesktop&hl=en)
 3. Copy the ID of the Google Sheet. In the example above it is `17eiV1NY78hISTCsygjlXGPw9G6v75yTB5vB0IzRvmjA`
@@ -17,4 +27,4 @@ Schedule the display of a set of URLs via a public Google Sheet.
 
 ## Notes
 
-Google Sheet will be polled every 10 minutes to check for updates.
+Google Sheet will be polled every 10 minutes to check for updates. If you are using Google Calendar and hosting this application on your own domain you will need to generate a new pubic Google Calendar API key and modify the value in `main.js`.
--- js/main.js
+++ js/main.js
@@ -1,34 +1,118 @@
-var params = new URLSearchParams(window.location.search);
-var defaultUrl = params.get('default') ? decodeURI(params.get('default')) : null;
-var sheetId = params.get('id');
-var scrolling = params.get('scroll');
+const PUBLIC_GCAL_API_KEY = "AIzaSyC02W8pskb4c54YeTJ2y3QSsgOiMOIpY2M"; // running this on your own domain? you'll need your own key.
+const GCAL_FETCH_HOURS_LIMIT = 24; // hours to look forward when fetching from Google Calendar
+const FETCH_INTERVAL = 15 * 60 * 1000; // fetch updates every 15 minutes
 
-var iframe;
+var defaultUrl, 
+    sheetId, 
+    calendarId,
+    scrolling, 
+    scheduledEntries,
+    nextTimeout,
+    iframe;
 
-document.getElementById('default').value = defaultUrl;
-document.getElementById('id').value = sheetId;
-document.getElementById('scroll').value = scrolling || 'no';
+init();
+$(start);
 
-if(!defaultUrl || !sheetId){
-    document.body.classList.remove('active');
+function init(){
+    let params = new URLSearchParams(window.location.search);
+    defaultUrl = params.get('default') ? decodeURI(params.get('default')) : null;
+    sheetId = params.get('sheet');
+    calendarId = params.get('calendar');
+    scrolling = params.get('scroll');
+
+    document.getElementById('default').value = defaultUrl;
+    document.getElementById('sheet').value = sheetId;
+    document.getElementById('calendar').value = calendarId;
+    document.getElementById('scroll').value = scrolling || 'no';
+
+    if(!defaultUrl || (!sheetId && !calendarId)){
+        document.body.classList.remove('active');
+    }
 }
 
-$(function(){
-    if(defaultUrl && sheetId){
+function start(){
+    if(defaultUrl && (sheetId || calendarId)){
         iframe = document.createElement('iframe');
         iframe.scrolling = scrolling;
+        document.body.appendChild(iframe);
 
-        var jsonUrl = "https://spreadsheets.google.com/feeds/list/" + sheetId + "/1/public/values?alt=json-in-script&callback=?";
-        $.getJSON(jsonUrl, function(data) {
-            var entries = data.feed.entry.map(function(entry){
-                return {
-                    start: Date.parse(entry['gsx$start']['$t']),
-                    url: ['gsx$url']['$t']
-                }
-            });
-            console.log(entries);
-            iframe.src = defaultUrl;
-            document.body.appendChild(iframe);
-        });
+        fetchSchedule();
+        setInterval(fetchSchedule, FETCH_INTERVAL);
     }    
-});
+}
+
+function updateContent(){
+    if(nextTimeout) clearTimeout(nextTimeout);
+    const now = (new Date()).getTime();
+    let currentEntry = {
+        start: 0
+    };
+    let nextUpdateTime;
+    for(let i = 0; i < scheduledEntries.length; i++){
+        const entry = scheduledEntries[i];
+        if(entry.start <= now && entry.start > currentEntry.start && (!entry.end || entry.end >= now)){
+            currentEntry = entry;
+            if(scheduledEntries[i + 1]){
+                nextUpdateTime = scheduledEntries[i + 1].start;
+            }
+        }
+    }
+    if(currentEntry.end && currentEntry.end < nextUpdateTime){
+        nextUpdateTime = currentEntry.end;
+    }
+    iframe.src = currentEntry.url || defaultUrl;
+    if(nextUpdateTime){
+        nextTimeout = setTimeout(updateContent, nextUpdateTime - now);
+    }
+}
+
+function fetchSchedule(){
+    const minEnd = new Date();
+    let maxStart = new Date(); 
+    maxStart.setHours(maxStart.getHours() + GCAL_FETCH_HOURS_LIMIT);
+    const fetches = [];
+    if(calendarId){
+        fetches.push(getCalendar(minEnd, maxStart));
+    }
+    if(sheetId){
+        fetches.push(getSheet(minEnd, maxStart));
+    }
+    Promise.all(fetches).then(function(entries){
+        scheduledEntries = entries.flat().sort(function(a, b) {
+            return a.start - b.start;
+        });
+        console.log(minEnd.getTime(), 'scheduled entries', scheduledEntries);
+        updateContent();
+    });
+}
+
+function getCalendar(minEnd, maxStart){
+    const url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarId 
+        + "/events?key=" + PUBLIC_GCAL_API_KEY 
+        + "&orderBy=startTime&singleEvents=true"
+        + "&timeMin=" + minEnd.toISOString()
+        + "&timeMax=" + maxStart.toISOString();
+    return $.getJSON(url).then(function(data) {
+        return data.items.map(function(entry){
+           return {
+               start: Date.parse(entry.start.dateTime || entry.start.date),
+               end:  Date.parse(entry.end.dateTime || entry.end.date),
+               url: entry.summary
+           }
+        });
+    });
+}
+
+function getSheet(minEnd, maxStart){
+    const url = "https://spreadsheets.google.com/feeds/list/" + sheetId + "/1/public/values?alt=json-in-script&callback=?";
+    return $.getJSON(url).then(function(data) {
+        return data.feed.entry.map(function(entry){
+            return {
+                start: Date.parse(entry['gsx$start']['$t']),
+                url: ['gsx$url']['$t']
+            };
+        }).filter(function(entry){
+            return entry.start <= maxStart.getTime()
+        });
+    });
+}
